76abdd50acbafc7cab977a0466a9b8e413a68c11
andrei <andrei98cmp@gmail.com>
2023-03-23 12:38:04 +0200

    commit

--- src/main/java/AreaOfTriangle.java
+++ src/main/java/AreaOfTriangle.java
@@ -1,5 +1,8 @@
 public class AreaOfTriangle {
     public static double calculateTriangleArea(double base, double height) {
+        if (base <= 0 || height <= 0) {
+            throw new IllegalArgumentException("Base and height must be positive");
+        }
         return 0.5 * base * height;
     }
 }
