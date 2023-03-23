337ad1881c6397d09fc53b1bf8da2ca2f7debeda
andrei <andrei98cmp@gmail.com>
2023-03-23 12:20:32 +0200

    commit

--- src/test/java/CombinedTest.java
+++ src/test/java/CombinedTest.java
@@ -8,14 +8,14 @@ import static org.junit.Assert.assertEquals;
 
 public class CombinedTest {
 
-        @Test
-        public void testAreaOfTriangle() {
-            double base = 4.0;
-            double height = 3.0;
-            double expectedArea = 6.0;
-            double actualArea = AreaOfTriangle.calculateTriangleArea(base, height);
-            assertEquals(expectedArea, actualArea, 0.001);
-        }
+    @Test
+    public void testAreaOfTriangle() {
+        double base = 4.0;
+        double height = 3.0;
+        double expectedArea = 6.0;
+        double actualArea = AreaOfTriangle.calculateTriangleArea(base, height);
+        assertEquals(expectedArea, actualArea, 0.001);
+    }
 
     @Test
     public void testPrintNumbers() {
